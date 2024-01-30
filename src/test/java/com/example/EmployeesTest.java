a6b98dbebeeb83173ad6b6d85e7f263ac569933a
Emmelie83 <info@emmeliejohansson.com>
2024-01-30 12:30:21 +0100

    Add test for when exception is thrown

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -3,7 +3,13 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 class EmployeesTest {
     EmployeeRepositoryTestDouble employeeRepository;
@@ -32,6 +38,25 @@ class EmployeesTest {
         int payments = employees.payEmployees();
 
         assertEquals(0, payments);
+    }
+
+    @Test
+    void givenExceptionIsThrownThenPayEmployeesShouldReturn0() {
+        BankService bankService = mock(BankService.class);
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        employees = new Employees(employeeRepository, bankService);
+
+        List<Employee> mockEmployees = Arrays.asList(
+                new Employee("1", 45000.0),
+                new Employee("2", 58000.0)
+        );
+
+        when(employeeRepository.findAll()).thenReturn(mockEmployees);
+        doThrow(new RuntimeException("Payment failed")).when(bankService).pay(anyString(), anyDouble());
+        int payments = employees.payEmployees();
+
+        assertEquals(0, payments);
+        verify(bankService, times(2)).pay(anyString(), anyDouble());
     }
 
 }
