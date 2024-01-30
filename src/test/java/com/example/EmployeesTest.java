d30e97df0902aede4dbd13898387b3f72768c401
Emmelie83 <info@emmeliejohansson.com>
2024-01-30 12:22:50 +0100

    Add test for payEmployees with two employees

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,6 +1,26 @@
 package com.example;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class EmployeesTest {
+    EmployeeRepositoryTestDouble employeeRepository;
+    BankServiceTestDouble bankService;
+    Employees employees;
+
+    @Test
+    void givenTwoEmployeesThenPayEmployeesShouldReturn2() {
+        employeeRepository = new EmployeeRepositoryTestDouble();
+        bankService = new BankServiceTestDouble();
+        employees = new Employees(employeeRepository, bankService);
+        employeeRepository.save(new Employee("1", 45000.0));
+        employeeRepository.save(new Employee("2", 58000.0));
+
+        int payments = employees.payEmployees();
+
+        assertEquals(2, payments);
+    }
 
 }
